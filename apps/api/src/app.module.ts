ce918e26d5c5ac4560d9bfd25528d605c9df005f
ara3isc <m.aneddame01@gmail.com>
2026-08-17 15:56:44 +0100

    fix: enforce active invitation uniqueness

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
+import { HealthController } from './health/health.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 import { AccessModule } from './access/access.module';
 import { AuthModule } from './auth/auth.module';
-import { HealthController } from './health/health.controller';
-import { PrismaModule } from './prisma/prisma.module';
+import { InvitationModule } from './invitation/invitation.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
     AccessModule,
+    InvitationModule,
   ],
   controllers: [HealthController],
 })
